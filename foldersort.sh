16681625991507cc18ca2b034e36c2306d134e9f
Erik Johnson <github@ekriirke.net>
2025-03-09 18:41:58 +0100

    Update foldersort.sh
    Try to determine current launcher database

--- foldersort.sh
+++ foldersort.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #Erik Johnson - EkriirkE 2022-05-17
-db="/data/data/com.android.launcher3/databases/launcher.db"
+
+trebuchet="/data/data/com.android.launcher3"
+grid=$(sed -n 's:.*<string name="idp_grid_name">\(.*\)</string>.*:\1:p' "$trebuchet/shared_prefs/com.android.launcher3.prefs.xml")
+if [ ! -z "$grid" ]; then grid="_$grid"; else grid=""; fi
+echo Using launcher$grid
+db="$trebuchet/databases/launcher$grid.db"
 
 read mx my <<< $(sqlite3 -tabs $db "SELECT MAX(cellX),MAX(cellY) FROM favorites WHERE container>0;")
 echo Using folder grid of $mx"x"$my
@@ -24,4 +29,3 @@ sqlite3 -tabs $db "SELECT c.title,i._id,i.title FROM favorites i INNER JOIN favo
 	((k=k+1))
 done
 pkill launcher3
-
